d105cc58714110d0290ec1ae2242da2abe558a22
Anna Merkulova <panechek@inbox.ru>
2023-10-25 10:32:52 +0300

    add word filter

--- frontend/src/components/Messages.jsx
+++ frontend/src/components/Messages.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import filter from 'leo-profanity';
 
-const Messages = ({ messages }) => (
-    <>{messages.map((message) => (
-        <div key={message.id} className='text-break mb-2'>
+const Messages = ({ messages }) => {
+  filter.list();
+  filter.clearList();
+
+  filter.add(filter.getDictionary('en'));
+  filter.add(filter.getDictionary('fr'));
+  filter.add(filter.getDictionary('ru'));
+
+  filter.list();
+  return (
+      <>
+        {messages.map((message) => (
+          <div key={message.id} className='text-break mb-2'>
             <b>{message.username}</b>
-            : {message.value}
-        </div>
-    ))}
-    </>
-);
+            : {filter.clean(message.value)}
+          </div>
+        ))}
+      </>
+  );
+};
 
 export default Messages;
