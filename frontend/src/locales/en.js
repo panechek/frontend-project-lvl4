a0176db933b939d522de31c202cf7f807d6fb2d5
Anna Merkulova <panechek@inbox.ru>
2023-10-26 12:56:52 +0300

    fix npm

--- frontend/src/locales/en.js
+++
@@ -1,5 +0,0 @@
-export default {
-  translation: {
-    'Welcome to React': 'Welcome to React and react-i18next',
-  },
-};
--- frontend/src/locales/index.js
+++ frontend/src/locales/index.js
@@ -1,9 +1,8 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import ru from './ru.js';
-import en from './en.js';
 
-const resources = { ru, en };
+const resources = { ru };
 
 i18n
   .use(initReactI18next)
